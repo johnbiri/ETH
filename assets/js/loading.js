1b6178f490e2995e95c9c888f2dbe81b6484b398
John Biri <johnhf12345@gmail.com>
2024-10-16 21:46:39 -0300

    first commit

---
+++ assets/js/loading.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+  // Ocultar a tela de carregamento
+  document.getElementById('loading-screen').style.display = 'none';
+  
+  // Mostrar o conteúdo da página
+  document.getElementById('content').style.display = 'block';
+});
---
+++ z - readme.txt
@@ -0,0 +1,6 @@
+Página ETH de John Biri
+
+- Um guia.
+- Um palácio mental.
+
+Obrigado por fazer parte dessa jornada! 😊
